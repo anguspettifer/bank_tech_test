d9e0f9b7eb0a83d3a6e7b9da6da239a5942249bb
anguspettifer <angus.pettifer@gmail.com>
2018-07-16 15:31:30 +0100

    Refactor date method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,9 +2,9 @@ require 'date'
 
 class Transaction
 
-  def initialize(account, date = Date)
+  def initialize(account, date_class = Date)
     @account = account
-    @date_class = date
+    @date_class = date_class
   end
 
   def deposit(ammount)
@@ -16,14 +16,31 @@ class Transaction
   end
 
   def date
-    month_hash = {
-      7 => "JUL"
-    }
     day = @date_class.today.day
     month_int = @date_class.today.month
-    month_str = month_hash[month_int]
+    month_str = month_format(month_int)
     year = @date_class.today.year
     formatted_date = "#{day}-#{month_str}-#{year}"
+  end
+
+  private
+
+  def month_format(month_int)
+    month_hash = {
+      1 => "JAN",
+      2 => "FEB",
+      3 => "MAR",
+      4 => "APR",
+      5 => "MAY",
+      6 => "JUN",
+      7 => "JUL",
+      8 => "AUG",
+      9 => "SEP",
+      10 => "OCT",
+      11 => "NOV",
+      12 => "DEC"
+    }
+    month_str = month_hash[month_int]
   end
 
 end
