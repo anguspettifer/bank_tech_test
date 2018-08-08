71d211f1f372892db211a155cbcec501e5aff37d
anguspettifer <angus.pettifer@gmail.com>
2018-08-08 11:11:00 +0100

    Use attr_reader method in transaction class
    * Prioritizing behaviour over state

--- lib/formatted_transaction.rb
+++ lib/formatted_transaction.rb
@@ -2,15 +2,18 @@
 
 # Understands how to format a transaction
 class FormattedTransaction
+  attr_reader :transaction
+
   def initialize(transaction)
     @transaction = transaction
   end
 
   def create_format
-    date = format_date(@transaction.date)
-    type = @transaction.type
-    amount = @transaction.amount
-    balance = @transaction.balance
+    # is this method doing too much?
+    date = format_date(transaction.date)
+    type = transaction.type
+    amount = transaction.amount
+    balance = transaction.balance
     credit_amount = format('%.2f', amount) if type == :credit
     debit_amount = format('%.2f', amount) if type == :debit
     balance = format('%.2f', balance)
